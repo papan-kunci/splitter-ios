d3999cee90343fcf64fc7b6cc96592f8a1ac7ef7
Nicolas Lukita <nicolas.lukita@synpulse.com>
2024-01-24 22:48:15 +0800

    Initial Commit

---
+++ Splitter/SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitterApp.swift
+//  Splitter
+//
+//  Created by Nicolas Lukita on 2024/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
