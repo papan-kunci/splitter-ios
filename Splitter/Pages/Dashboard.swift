8f7cc2af3f489e258d3d6ccba884741b9e51b070
Nicolas Lukita <nicolas.lukita@synpulse.com>
2024-02-29 17:36:37 +0800

    refactor: remove unused components

--- Splitter/Pages/Dashboard.swift
+++ Splitter/Pages/Dashboard.swift
@@ -10,7 +10,6 @@ import SwiftUI
 enum Tab: String, CaseIterable {
     case friends
     case groups
-    case plus
     case activity
     case account
 }
@@ -21,82 +20,13 @@ struct ScaleButtonStyle: ButtonStyle {
             .frame(width: 60, height: 60)
             .background(Color.mint)
             .clipShape(RoundedRectangle(cornerRadius: configuration.isPressed ? 24 : 20))
-            .shadow(radius: configuration.isPressed ? 0 : 4)
+            .shadow(radius: configuration.isPressed ? 0 : 2)
             .scaleEffect(configuration.isPressed ? 0.99 : 1)
-    }
-}
-
-struct TabBar: View {
-    @Binding var selectedTab: Tab
-    @State var isButtonPressed = false
-    var body: some View {
-        ZStack {
-            HStack {
-                ForEach(Tab.allCases, id:\.rawValue) { tab in
-                    Spacer()
-                    
-                    if (tab == .plus) {
-                        Spacer()
-                    } else {
-                        Text(tab.rawValue.prefix(3))
-                            .fontWeight(tab == selectedTab ? .semibold : .regular)
-                            .scaleEffect(tab == selectedTab ? 1.10 : 1.0)
-                            .foregroundStyle(tab == selectedTab ? .blue : .gray)
-                            .onTapGesture {
-                                withAnimation(.easeIn(duration: 0.1)) {
-                                    selectedTab = tab
-                                }
-                            }
-                    }
-                    
-                    Spacer()
-                }
-            }
-            .frame(width: nil, height: 45)
-            .background(.thinMaterial)
-            .cornerRadius(10)
-            .padding(EdgeInsets(top: 6, leading: 0, bottom: 0, trailing: 0))
-            
-            Button {
-                isButtonPressed.toggle()
-            } label: {
-                Image(systemName: "plus")
-                    .resizable()
-                    .frame(width: 20, height: 20)
-                    .foregroundColor(.white)
-            }.buttonStyle(ScaleButtonStyle())
-            
-            
-        }
     }
 }
 
 struct Dashboard: View {
     @State private var selectedTab: Tab = .account
-    var body: some View {
-        ZStack {
-            VStack {
-                TabView(selection: $selectedTab) {
-                    FriendsPage()
-                        .tag(Tab.friends)
-                    GroupsPage()
-                        .tag(Tab.groups)
-                    ActivityPage()
-                        .tag(Tab.activity)
-                    AccountPage()
-                        .tag(Tab.account)
-                }
-            }
-            VStack {
-                Spacer()
-                TabBar(selectedTab: $selectedTab)
-            }
-        }
-    }
-}
-
-struct OldStyleDashboard: View {
-    @State private var selectedTab: Tab = .friends
     @State var isButtonPressed = false
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -129,6 +59,5 @@ struct OldStyleDashboard: View {
 }
 
 #Preview {
-    //    Dashboard()
-    OldStyleDashboard()
+        Dashboard()
 }
